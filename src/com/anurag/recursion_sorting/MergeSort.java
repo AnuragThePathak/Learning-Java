68d6a3091543dfe55e3b47157fecc6c91a3769bd
Anurag Pathak <anuragpathak911@gmail.com>
2021-12-02 16:15:40 +0530

    Merge sort.

---
+++ src/com/anurag/recursion_sorting/MergeSort.java
@@ -0,0 +1,48 @@
+package com.anurag.recursion_sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = { 2, 1, 6, 3, 0, 9, 5};
+        System.out.println(Arrays.toString(sort(arr)));
+    }
+
+    static int[] sort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+
+        int[] left = sort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = sort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right, arr.length);
+    }
+
+    static int[] merge(int[] left, int[] right, int n) {
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        int[] arr = new int[n];
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                arr[index++] = left[i++];
+            } else {
+                arr[index++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            arr[index++] = left[i++];
+        }
+
+        while (j < right.length) {
+            arr[index++] = right[j++];
+        }
+
+        return arr;
+    }
+}
