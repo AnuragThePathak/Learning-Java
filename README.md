e6903990acf8daa37046b547751c335d71b110c2
Anurag Pathak <anuragpathak911@gmail.com>
2021-12-30 18:19:17 +0530

    README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Java
+
+It contains the codes I have written in while learning Java as well as some of Data Structures and Algorithms. I mainly followed [Kunal Kushwaha's Bootcamp](https://youtube.com/playlist?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ) to learn Java and DSA hence the topics included here is also similar to that playlist itself.
--- src/com/anurag/recursion_sorting/MergeSort.java
+++ src/com/anurag/recursion_sorting/MergeSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MergeSort {
 
     public static void main(String[] args) {
-        int[] arr = { 2, 1, 6, 3, 0, 9, 5};
+        int[] arr = { 2, 1, 6, 3, 0, 9, 5, 4, -1, -2};
         System.out.println(Arrays.toString(sort(arr)));
     }
 
@@ -19,14 +19,14 @@ public class MergeSort {
         int[] left = sort(Arrays.copyOfRange(arr, 0, mid));
         int[] right = sort(Arrays.copyOfRange(arr, mid, arr.length));
 
-        return merge(left, right, arr.length);
+        return merge(left, right);
     }
 
-    static int[] merge(int[] left, int[] right, int n) {
+    static int[] merge(int[] left, int[] right) {
         int i = 0;
         int j = 0;
         int index = 0;
-        int[] arr = new int[n];
+        int[] arr = new int[left.length + right.length];
 
         while (i < left.length && j < right.length) {
             if (left[i] < right[j]) {
